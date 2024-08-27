cabb2beb1210ee8450cfbbcefad60d6a997252b4
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-08-27 19:29:04 +0400

    init: with CardTest imported from Library of It-Leaders (Temporary)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import "library_it_leaders_temporary/css"
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({Component, pageProps}: AppProps) {
+    return <Component {...pageProps} />;
 }
